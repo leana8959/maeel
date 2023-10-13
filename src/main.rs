68f2e225fdea07ef27260b976be43006562f41f5
b2o <b2oba@dnmx.org>
2023-10-13 18:26:34 +0200

    Added supports for tabs indents

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::fs::read_to_string;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut vm = vm::BocchiVM::default();
-
+ 
     vm.process_tokens(
         &mut lex_into_tokens(&read_to_string(args().nth(1).unwrap())?),
         &mut HashMap::default(),
