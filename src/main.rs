537f1838bcc18c928d42ad615da513f9d3d56dbe
toast <58362380+Traumatism@users.noreply.github.com>
2024-01-21 12:03:07 +0100

    Multiple edits, should start working on doc soon

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,7 @@ use std::env::args;
 use std::fs::read_to_string;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut vm = vm::BocchiVM::default();
-
-    vm.process_tokens(
+    vm::BocchiVM::default().process_tokens(
         &mut lex_into_tokens(&read_to_string(args().nth(1).unwrap())?),
         &mut HashMap::default(),
         &mut HashMap::default(),
